c4f05d901ec8d20440e3ceef9f58cb945b240595
ola <ola@ola.pl>
2023-03-26 17:55:41 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Aleksandra Łepkowska on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
